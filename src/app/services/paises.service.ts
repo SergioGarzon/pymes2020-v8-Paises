fca62f5fba14b16bf0206d2cb0caac2429a0538c
SergioGarzon <sggarzonutn@gmail.com>
2020-11-30 18:14:06 -0300

    1 file modified

--- src/app/services/paises.service.ts
+++ src/app/services/paises.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { Pais } from "../models/pais";
 
-@Injectable()
+@Injectable({
+  providedIn: "root"
+})
 export class PaisesService {
+  resourceUrl: string;
+  constructor(private httpClient: HttpClient) {
+    this.resourceUrl = "https://pav2.azurewebsites.net/api/paises/";
+  }
 
-  constructor() { }
+  get() {
+    return this.httpClient.get(this.resourceUrl);
+  }
 
+  getById(Id: number) {
+    return this.httpClient.get(this.resourceUrl + Id);
+  }
+
+  post(obj: Pais) {
+    return this.httpClient.post(this.resourceUrl, obj);
+  }
+  
 }
